f44da412b056935250af7fde235d1a2c7d5abcca
Juan Castro <juamcastroh@gmail.com>
2026-05-21 04:12:51 -0300

    feat: Registrar routes en main

--- auth_service/app/main.py
+++ auth_service/app/main.py
@@ -3,17 +3,23 @@
 # ===================================
 
 # --- Obtener Framework ---
-from fastapi import FastAPI
+from fastapi import FastAPI 
 
+# --- Traer Base ORM y engine (puente) ---
 from auth_service.app.database.connection import engine
 from auth_service.app.database.base import Base
 
-from auth_service.app.models.user_model import User
+# --- Registrar modelo ORM User en metadata ---
+from auth_service.app.models.user_model import User # Python ejecuta el archivo al importarlo. 
 
+from auth_service.app.routes.auth_routes import router as auth_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# --- Incluir router a app ---
+app.include_router(auth_router)
 
 # Ruta Inicial de prueba
 @app.get("/")
