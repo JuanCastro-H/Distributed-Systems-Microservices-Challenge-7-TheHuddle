36df4ecfce63526ba39564f8a629e4e8026358d1
Juan Castro <juamcastroh@gmail.com>
2026-05-21 04:26:26 -0300

    feat: Agregar metodo para login de usuarios

--- auth_service/app/services/auth_service.py
+++ auth_service/app/services/auth_service.py
@@ -15,6 +15,12 @@ from auth_service.app.repositories.user_repository import UserRepository
 # --- Importar funcion de Hashing ---
 from auth_service.app.core.security import hash_password
 
+# --- Funcion para comparar y verificar contrasenha ---
+from auth_service.app.core.security import verify_password
+
+# --- Generador de tokens ---
+from auth_service.app.core.security import create_access_token
+
 
 
 # --- Servicio con logica de autenticacion y registro ---
@@ -49,3 +55,41 @@ class AuthService:
             email,
             hashed_pw
         )
+    
+        # --- Login De Usuarios ---
+    @staticmethod
+    def login_user(
+        db: Session,
+        email: str,
+        password: str
+    ):
+        # --- Buscar usuario por Email ---
+        user = UserRepository.get_user_by_email(
+            db,
+            email
+        )
+
+        # --- Si no lo encontramos devolver None ---
+        if not user:
+
+            return None
+
+        # --- Verificar contrasenha ---
+        valid_password = verify_password(
+            password,             # Hashea Contrasnha Para compararla con el hash de la contrasenha real.
+            user.hashed_password  # Hash de la Contrasenha real.
+        )
+
+        # --- Si no es valida devolver None ---
+        if not valid_password:
+
+            return None
+        
+        # --- Al logearse correctamente creamos un JWT ---
+        access_token = create_access_token({
+            "sub": str(user.id)
+        })
+
+        # --- Retornar el token ---
+        return access_token
+    
