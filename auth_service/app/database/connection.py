3ca05d0cfaa79aaded4790647715a3ff611a5f5c
Juan Castro <juamcastroh@gmail.com>
2026-05-21 03:04:20 -0300

    refactor/feat: Agregar carpeta app para orginzacion modular, y crear archivo dependencies.py dentro del modulo de database para crear sesiones con la base de datos

--- auth_service/app/database/connection.py
+++ auth_service/app/database/connection.py
@@ -11,16 +11,27 @@ import os # Permite Interaccion con OS
 from sqlalchemy import create_engine    # Administrador de conexiones con PostgreSQL
 from sqlalchemy.orm import sessionmaker # Plantilla de sesiones
 
+from pathlib import Path                # Manejar rutas
 from dotenv import load_dotenv          # Lector de archivos env
 
+
+
+# --- Obtener ruta absoluta del archivo actual ---
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# --- Construir ruta al .env ---
+env_path = BASE_DIR / ".env"
+
 # --- Leer Variables de entorno ---
-load_dotenv("auth_service/.env")
+load_dotenv(env_path)
 
 # --- Obtener URL de la BD ---
 DATABASE_URL = os.getenv("DATABASE_URL") 
 
+
 # --- Crear la conexion/puente con PostgreSQL ---
 engine = create_engine(DATABASE_URL)
+
 
 # --- Crear Plantilla de Sesiones para BD --
 SessionLocal = sessionmaker(
---
+++ auth_service/app/database/dependencies.py
@@ -0,0 +1,25 @@
+# =================================================================
+# MODULO DE DEPENDENCIA 
+# Crea una conexion temporal con la BD mientras dure una request.
+# =================================================================
+
+# ===================================
+# Importaciones Y Librerias
+# ===================================
+
+from auth_service.app.database.connection import SessionLocal # Plantilla de Sesiones para la base de datos.
+
+# --- Crear Session De BD Inyectable ---
+def get_db():
+
+    # --- Crear una nueva sesion SQLAlchemy ---
+    db = SessionLocal()
+
+    try:     # Intenta.    
+
+        yield db # Pausa la funcion, y entrega elementos a demanda.
+    
+    finally: # Bloque de limpieza.
+
+        # --- Cerrar conexion con la BD ---
+        db.close()
