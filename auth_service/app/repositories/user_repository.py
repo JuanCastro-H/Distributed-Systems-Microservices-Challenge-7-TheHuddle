03a3c53248ae7fa3b86c213661115ac980c8b788
Juan Castro <juamcastroh@gmail.com>
2026-05-21 04:03:56 -0300

    feat: agregar metodo para crear usuarios nuevos al repositorio

---
+++ auth_service/app/repositories/user_repository.py
@@ -0,0 +1,61 @@
+# ====================================
+# REPOSITORIO DE USUARIOS
+# Encapsula y centraliza consultas SQL.
+# ====================================
+
+# ===================================
+# Importaciones Y Librerias
+# ===================================
+
+# --- Importar Session ---
+from sqlalchemy.orm import Session # Obtenemos la conexion activa con la BD
+
+# --- Importar modelo de User ---
+from auth_service.app.models.user_model import User
+
+
+# --------------------------------------------
+# Centralizar consutas SQL de usuarios
+# --------------------------------------------
+class UserRepository:
+
+    # --- Obtener email de la base de datos ---
+    @staticmethod # Permite usar el metodo de la clase sin crear un objeto.
+    def get_user_by_email(
+        db: Session,
+        email: str
+    ):
+        # --- Devolver email ---
+        return db.query(User).filter(
+            User.email == email
+        ).first() # Devuelve el primer resultado o None
+    
+
+        # --- Crear un usuario nuevo ---
+    @staticmethod
+    def create_user(
+
+        # --- Obtener datos procesados ---
+        db: Session,
+        username: str,
+        email: str,
+        hashed_password: str
+    ):
+        # --- Cargar datos a un objeto usurio ---
+        user = User(
+            username=username,
+            email=email,
+            hashed_password=hashed_password
+        )
+
+        # --- Prepara el objeto para insertarlo ---
+        db.add(user)
+
+        # --- Guardar en la BD ---
+        db.commit()
+
+        # --- Cargar Id al usuario ---
+        db.refresh(user)
+        
+        # --- Retornar usuario nuevo --
+        return user
