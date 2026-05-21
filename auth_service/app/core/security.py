4cd8ea3c7428e41db16081509788f1708fc89fa1
Juan Castro <juamcastroh@gmail.com>
2026-05-21 04:20:42 -0300

    feat: Agregar funcion para crear JWT

--- auth_service/app/core/security.py
+++ auth_service/app/core/security.py
@@ -8,6 +8,42 @@
 
 from passlib.context import CryptContext # Administrador de algoritmos de hashing
 
+import os # Interactuar con el OS
+from pathlib import Path  
+
+# --- Manejo de fechas y expiracion de tokens JWT ---
+from datetime import datetime
+from datetime import timedelta
+from datetime import timezone
+
+# --- Libreria para crear y verificar JWT ---
+from jose import jwt
+
+# --- Manejo de errores JWT invalidos ---
+from jose import JWTError
+
+# --- Leer archivos env
+from dotenv import load_dotenv
+
+
+
+# --- Obtener ruta absoluta del archivo actual ---
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# --- Construir ruta al .env ---
+env_path = BASE_DIR / ".env"
+
+# --- Leer Variables de entorno ---
+load_dotenv(env_path)
+
+
+# --- Obtener variables de entorno ---
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+ALGORITHM = os.getenv("ALGORITHM")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 
 # --- Configuracion del contenedor de Hashin ---
@@ -32,3 +68,28 @@ def verify_password(
         plain_password,
         hashed_password
     )
+
+
+# --- Crear acceso al token ---
+def create_access_token(data: dict):
+
+    # --- Copiar dtos del payload ---
+    to_encode = data.copy()
+
+    # --- Crear fecha de expircion del token ---
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    # --- agregar Expiracion al JWT ---
+    to_encode.update({
+        "exp": expire
+    })
+
+    # --- Crear JWT ---
+    encoded_jwt = jwt.encode(  # Codifica el token.
+        to_encode,             # Payload / datos que iran dentro del token.
+        SECRET_KEY,            # Clave secreta sirve para firmar el token y verificar autenticidad.
+        algorithm=ALGORITHM 
+    )
+
+    # --- Devolver JWT ---
+    return encoded_jwt
