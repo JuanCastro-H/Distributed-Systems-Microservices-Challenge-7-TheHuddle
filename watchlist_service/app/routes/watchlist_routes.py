dccb7df0f21653733ddcf21b5bc9a5be8b184494
Juan Castro <juamcastroh@gmail.com>
2026-05-22 12:54:11 -0300

    docs: Agregar comentarios al modulo de rutas

--- watchlist_service/app/routes/watchlist_routes.py
+++ watchlist_service/app/routes/watchlist_routes.py
@@ -1,24 +1,42 @@
+# ====================================
+# MODULO DE RUTAS
+# ====================================
+
+# ============================
+# Importaciones Y Librerias
+# ============================
+
+# --- Importar Rauter y obtener dependencias ---
 from fastapi import APIRouter
 from fastapi import Depends
 
+# --- Obtener Session activa con la BD ---
 from sqlalchemy.orm import Session
 
+# --- Obtener Schemas de validacion y respuesta ---
 from app.schemas.watchlist_schema import WatchlistCreate
 from app.schemas.watchlist_schema import WatchlistResponse
 
+# --- Obtener conexion con la BD ---
 from app.database.dependencies import get_db
 
+# --- Obtener Metodos de Watchlist ---
 from app.services.watchlist_service import WatchlistService
 
+# --- Obtener funcion para verificar JWT ---
 from app.core.security import verify_token
 
 
+
+# --- Crear Router ---
 router = APIRouter(
     prefix="/watchlist",
     tags=["Watchlist"]
 )
 
-
+# -------------------------------------------
+# RUTA PARA AGREGAR PELICULAS A WATCHLIST
+# -------------------------------------------
 @router.post("",response_model=WatchlistResponse)
 def add_to_watchlist(
     watchlist_data: WatchlistCreate,
@@ -33,6 +51,9 @@ def add_to_watchlist(
     )
 
 
+# -------------------------------------------
+# RUTA PARA OBTENER WATCHLIST
+# -------------------------------------------
 @router.get("",response_model=list[WatchlistResponse])
 def get_watchlist(
     user_id: str = Depends(verify_token),
@@ -44,7 +65,9 @@ def get_watchlist(
         int(user_id)
     )
 
-
+# -------------------------------------------
+# RUTA PARA ELIMINAR UNA PELICULA POR SU ID
+# -------------------------------------------
 @router.delete("/{movie_id}")
 def remove_from_watchlist(
     movie_id: int,
