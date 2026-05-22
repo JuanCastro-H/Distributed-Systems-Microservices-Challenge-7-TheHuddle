7a6047f529982ff306cf435b9eb7a842ebac1bce
Juan Castro <juamcastroh@gmail.com>
2026-05-22 09:12:29 -0300

    feat: Crear funcion para manejo de sesiones con la BD y ruta tipo post para la introduccion de peliculas

---
+++ catalog_service/app/routes/movie_routes.py
@@ -0,0 +1,62 @@
+# --- Importar enrutador y manejo de errores ---
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+
+# --- importar sesion actual ---
+from sqlalchemy.orm import Session
+
+# --- Importar tabla de movies ---
+from app.models.movie_model import Movie
+
+# --- Obtener schema para la creacion y respuesta de una movie ---
+from app.schemas.movie_schema import MovieCreate
+from app.schemas.movie_schema import MovieResponse
+
+# --- Traer plantilla de sesiones ---
+from app.database.connection import SessionLocal
+
+# --- Traer jwt ---
+from app.core.security import verify_token
+
+# --- Inicializar Router ---
+router = APIRouter(
+    prefix="/catalog",
+    tags=["Catalog"]
+)
+
+# --- Obtener BD
+def get_db():
+
+    db = SessionLocal()
+
+    try:
+
+        yield db
+
+    finally:
+
+        db.close()
+
+
+# --- Introducir peliculas a la plataforma ---
+@router.post("/movies",response_model=MovieResponse)
+def create_movie(
+    movie_data: MovieCreate,
+    user_id: str = Depends(verify_token),
+    db: Session = Depends(get_db)
+):
+
+    movie = Movie(
+        title=movie_data.title,
+        genre=movie_data.genre,
+        rating=movie_data.rating
+    )
+
+    db.add(movie)
+
+    db.commit()
+
+    db.refresh(movie)
+
+    return movie
