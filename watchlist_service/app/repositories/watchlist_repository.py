3a8020e52561da9c5404a2dca03ba42bde86b313
Juan Castro <juamcastroh@gmail.com>
2026-05-22 10:15:44 -0300

    feat: Encapsular y centralizar consultas SQL

---
+++ watchlist_service/app/repositories/watchlist_repository.py
@@ -0,0 +1,38 @@
+from sqlalchemy.orm import Session
+
+from app.models.watchlist_model import Watchlist
+
+
+class WatchlistRepository:
+
+
+    @staticmethod
+    def create_watchlist_item(
+        db: Session,
+        user_id: int,
+        movie_id: int
+    ):
+
+        watchlist = Watchlist(
+            user_id=user_id,
+            movie_id=movie_id
+        )
+
+        db.add(watchlist)
+
+        db.commit()
+
+        db.refresh(watchlist)
+
+        return watchlist
+
+
+    @staticmethod
+    def get_user_watchlist(
+        db: Session,
+        user_id: int
+    ):
+
+        return db.query(Watchlist).filter(
+            Watchlist.user_id == user_id
+        ).all()
