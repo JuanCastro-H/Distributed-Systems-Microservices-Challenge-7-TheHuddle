4b9a6fb8ecab0c2d9196206cfa5ec4d0193a9282
Juan Castro <juamcastroh@gmail.com>
2026-05-22 13:35:47 -0300

    docs: Agregar comentarios a main

--- watchlist_service/app/main.py
+++ watchlist_service/app/main.py
@@ -1,20 +1,30 @@
+# ===================================
+# Importaciones Y Librerias
+# ===================================
+
+# --- Obtener Framework ---
 from fastapi import FastAPI
 
+# --- Traer Base ORM y engine (puente) ---
 from app.database.base import Base
 from app.database.connection import engine
 
+# --- Registrar modelo ORM en metadata ---
 from app.models.watchlist_model import Watchlist
 
+# --- Importar router ---
 from app.routes.watchlist_routes import router as watchlist_router
 
-
+# --- Crear tablas registradas en metadata ---
 Base.metadata.create_all(bind=engine)
 
+# --- Iniciar Instacia de FastAPI ---
 app = FastAPI()
 
+# --- Incluir router a app ---
 app.include_router(watchlist_router)
 
-
+# Ruta Inicial de prueba
 @app.get("/")
 def root():
 
