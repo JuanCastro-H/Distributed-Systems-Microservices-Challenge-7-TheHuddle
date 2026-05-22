0aa2e6b2ed4f894b7032b331deb753a3c58d2de6
Juan Castro <juamcastroh@gmail.com>
2026-05-22 09:14:11 -0300

    feat: Crear ruta tipo get para obtener una pelicula por su id

--- catalog_service/app/routes/movie_routes.py
+++ catalog_service/app/routes/movie_routes.py
@@ -69,3 +69,23 @@ def get_movies(
 ):
 
     return db.query(Movie).all()
+
+
+@router.get("/movies/{movie_id}",response_model=MovieResponse)
+def get_movie(
+    movie_id: int,
+    db: Session = Depends(get_db)
+):
+
+    movie = db.query(Movie).filter(
+        Movie.id == movie_id
+    ).first()
+
+    if not movie:
+
+        raise HTTPException(
+            status_code=404,
+            detail="Movie not found"
+        )
+
+    return movie
