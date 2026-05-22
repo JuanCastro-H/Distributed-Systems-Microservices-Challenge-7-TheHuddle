b4bb3c230c00d7b4b35c98e7f85c9e847e5a78fa
Juan Castro <juamcastroh@gmail.com>
2026-05-22 09:35:26 -0300

    feat: Agregar ruta tipo delete para eliminar peliculas por su id

--- catalog_service/app/routes/movie_routes.py
+++ catalog_service/app/routes/movie_routes.py
@@ -89,3 +89,30 @@ def get_movie(
         )
 
     return movie
+
+
+@router.delete("/movies/{movie_id}")
+def delete_movie(
+    movie_id: int,
+    user_id: str = Depends(verify_token),
+    db: Session = Depends(get_db)
+):
+
+    movie = db.query(Movie).filter(
+        Movie.id == movie_id
+    ).first()
+
+    if not movie:
+
+        raise HTTPException(
+            status_code=404,
+            detail="Movie not found"
+        )
+
+    db.delete(movie)
+
+    db.commit()
+
+    return {
+        "message": "Movie deleted successfully"
+    }
