f119bc432806fd57ada6487ece08358093784c56
Juan Castro <juamcastroh@gmail.com>
2026-05-22 13:19:59 -0300

    docs: Agregar comentarios al modulo de creacion de modelo base ORM

--- watchlist_service/app/database/base.py
+++ watchlist_service/app/database/base.py
@@ -1,3 +1,12 @@
-from sqlalchemy.orm import declarative_base
+# ============================================
+# MODULO DE ESTRUCTURA BASE PARA CREAR TABLAS
+# ============================================
 
+
+# --- Importaciones Y Librerias ---
+from sqlalchemy.orm import declarative_base
+# ORM -> Object Relational Mapping 
+
+
+# --- Crear La Clase Madre Para Tablas ---
 Base = declarative_base()
