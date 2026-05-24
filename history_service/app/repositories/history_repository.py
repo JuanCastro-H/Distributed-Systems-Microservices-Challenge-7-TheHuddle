f68d0e1bb17ed3723523511e03378ad78d9fb4e8
Juan Castro <juamcastroh@gmail.com>
2026-05-23 21:23:09 -0300

    feat: Agregar logica para registrar peliculas en el historial y obtener todas las peliculas asociadas a un usuario

---
+++ history_service/app/repositories/history_repository.py
@@ -0,0 +1,64 @@
+# ====================================
+# REPOSITORIO DE HISTORIAL
+# Encapsula y centraliza consultas SQL.
+# ====================================
+
+# ===================================
+# Importaciones Y Librerias
+# ===================================
+
+# --- Importar Session ---
+from sqlalchemy.orm import Session
+
+
+# --- Importar modelo History ---
+from app.models.history_model import History
+
+
+
+# --------------------------------------------
+# Centralizar consultas SQL del historial
+# --------------------------------------------
+class HistoryRepository:
+
+
+    # --- Crear un registro en el Historial ---
+    @staticmethod
+    def create_history(
+        db: Session,
+        user_id: int,
+        movie_id: int
+    ):
+
+        # --- Crear objeto a registrar ---
+        history = History(
+            user_id=user_id,
+            movie_id=movie_id
+        )
+
+        # --- Preparar Guardado ---
+        db.add(history)
+
+        # --- Confirmar cambio --
+        db.commit() 
+
+        # --- Obtener ID ---
+        db.refresh(history)
+
+        # --- Retornar historial ---
+        return history
+
+
+    # --- Obtener historial del usuario ---
+    @staticmethod
+    def get_user_history(
+        db: Session,
+        user_id: int
+    ):
+
+        # --- Buscar registros relacionados al usuario ---
+        return db.query(History).filter(
+            History.user_id == user_id
+        ).all() # Devolverlos todos.
+
+
